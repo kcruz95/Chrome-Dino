2562c66868104e12afba4b5c8f3ecfcb5fa5403f
kcruz95 <kyle.cruz@hotmail.ca>
2022-08-17 23:40:46 -0700

    eventListeners added to make dino jump

--- dino.js
+++ dino.js
@@ -1,17 +1,26 @@
+import { incrementCustomProperty, setCustomProperty, getCustomProperty } from "./updateCustomsProperty.js";
+
 const dinoElem = document.querySelector("[data-dino");
 const JUMP_SPEED = 0.45;
-const GRAVITY = 0.11;
+const GRAVITY = 0.0015;
 const DINO_FRAME_COUNT = 2; //makes the dino "move"
 const FRAME_TIME = 100;
 
 let isJumping;
 let dinoFrame;
 let currentFrameTime;
+let yVelocity;
 
 export function setUpDino() {
-  isJumping = false;
+  isJumping = false; // if the dino is not jumping (false), the img animates
   dinoFrame = 0
   currentFrameTime = 0;
+  yVelocity = 0;
+  setCustomProperty(dinoElem, "--bottom", 0);
+  // allows dino to jump with the press of the space button
+  // removeEventListener comes first to reset upon losing
+  document.removeEventListener("keydown", onJump);
+  document.addEventListener("keydown", onJump);
 };
 
 export function updateDino(delta, speedScale) {
@@ -35,5 +44,23 @@ function handleRun(delta, speedScale) {
 };
 
 function handleJump(delta, speedScale) {
+  if (!isJumping)
+  return;
 
+  incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta);
+
+  if (getCustomProperty(dinoElem, "--bottom") <- 0) {
+    setCustomProperty(dinoElem, "--bottom", 0);
+    isJumping = false
+  };
+
+  yVelocity -= GRAVITY * delta;
 };
+
+function onJump(e) {
+  if (e.code !== "Space" || isJumping)
+  return;
+
+  yVelocity = JUMP_SPEED;
+  isJumping = true;
+}
